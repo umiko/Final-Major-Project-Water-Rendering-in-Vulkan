9d99c8d88dabc26d71a56dfc676ceb5108596fef
Robert Scholl <robertscholl.private@gmail.com>
2018-04-22 22:40:12 +0100

    Comments
    Added comments explaining the code and updated the gitignore to ignore the vscode directory

--- VulkanTutorial/helper.hpp
+++ VulkanTutorial/helper.hpp
@@ -12,8 +12,10 @@ const bool compare_extensions(VkExtensionProperties &extensionA, VkExtensionProp
 
 //this all goes to hell if not build for windows, because linux already supports that stuff
 #ifdef COLORMODE
+//windows likes to redefine standard functions. bad dog
 #define NOMINMAX
 #include <Windows.h>
+//this enables cmd to use virtual terminal stuff, like color codes and such
 DWORD enable_virtual_terminal() {
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (hOut == INVALID_HANDLE_VALUE)
@@ -36,12 +38,15 @@ DWORD enable_virtual_terminal() {
 }
 #endif
 
+//enumerations conveniently starting at the numbers where the color codes begin
 enum class console_colors_foreground { black = 30, red, green, yellow, blue, purple, cyan, white };
 enum class console_colors_background { black = 40, red, green, yellow, blue, purple, cyan, white };
+//the escape character to use those codes
 const char* esc_char = { "\x1b[" };
 const char  esc_color_end_char = 'm';
 const char	esc_color_reset_char = '0';
 
+//console output, if no color info is submitted it will assume default colors
 template <class T> void info(T value, console_colors_foreground foreground_color_code = console_colors_foreground::white, console_colors_background background_color_code = console_colors_background::black) {
 #ifdef _DEBUG
 #ifdef COLORMODE
@@ -57,18 +62,22 @@ template <class T> void info(T value, console_colors_foreground foreground_color
 #endif // !_DEBUG
 }
 
+//this will use the info function, but give it a predefined color
 template <class T> void warn(T value) {
 	info(value, console_colors_foreground::yellow);
 }
 
+//this will use the info function, but give it a predefined color
 template <class T> void err(T value) {
 	info(value, console_colors_foreground::red);
 }
 
+//this will use the info function, but give it a predefined color
 template <class T> void succ(T value) {
 	info(value, console_colors_foreground::green);
 }
 
+//this will read a file to a buffer and return this buffer
 std::vector<char> read_file(const std::string &filename) {
 	info("Reading file...");
 	std::ifstream file(filename, std::ios::ate | std::ios::binary);
